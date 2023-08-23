66639718091456bef8cdc64ab0ca0a014e2a03a6
abdiranman <abdirahman.abdi330@.com>
2023-08-23 19:17:15 -0400

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -47,6 +47,16 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Short URL not found");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
