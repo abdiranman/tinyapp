aa522bbeca86e170fd19291b59932459a15280ac
abdiranman <abdirahman.abdi330@.com>
2023-08-23 20:48:29 -0400

    Implemented login and logout using cookies

--- express_server.js
+++ express_server.js
@@ -7,44 +7,41 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  // ... Your URL database entries
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser()); // Use cookie-parser middleware
+app.use(cookieParser());
 
-// Function to generate a random string
 function generateRandomString() {
-  // ...
+  let randomStr = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomStr;
 }
 
-// Home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// Display list of URLs
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"] // Pass in the username
+    username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
 });
 
-// Display individual URL
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  const templateVars = {
-    id,
-    longURL,
-    username: req.cookies["username"] // Pass in the username
-  };
+  const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
 });
 
-// POST route to update a URL resource
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newLongURL = req.body.longURL;
@@ -52,19 +49,16 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-// POST route to delete a URL resource
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
 
-// GET route to display the form for creating new URLs
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// POST route to create a new URL resource
 app.post("/urls", (req, res) => {
   const randomID = generateRandomString();
   const longURL = req.body.longURL;
@@ -72,7 +66,6 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomID}`);
 });
 
-// Redirect short URLs to long URLs
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
@@ -83,15 +76,18 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// Display login form
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-// Handle login form submission
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
