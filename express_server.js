19f7f96605887031fa126bd18a80a41aecabc7ac
abdiranman <abdirahman.abdi330@.com>
2023-08-23 20:29:09 -0400

    cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser"); // Adding cookie-parser
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-// URL Database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // ... Your URL database entries
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser()); // Using cookie-parser middleware
+app.use(cookieParser()); // Use cookie-parser middleware
+
+// Function to generate a random string
+function generateRandomString() {
+  let randomStr = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomStr;
+}
 
 // Home page
 app.get("/", (req, res) => {
@@ -22,7 +30,7 @@ app.get("/", (req, res) => {
 
 // Display list of URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username }; // Pass username to template
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -30,7 +38,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  const templateVars = { id, longURL, username: req.cookies.username }; // Pass username to template
+  const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
 });
 
@@ -51,7 +59,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // GET route to display the form for creating new URLs
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", { username: req.cookies.username }); // Pass username to template
+  res.render("urls_new");
 });
 
 // POST route to create a new URL resource
@@ -73,7 +81,12 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// User login form
+// Display login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Handle login form submission
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
@@ -83,13 +96,3 @@ app.post("/login", (req, res) => {
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
-
-// Function to generate a random string
-function generateRandomString() {
-  let randomStr = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 6; i++) {
-    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return randomStr;
-}
