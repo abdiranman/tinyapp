6307a5b9fd8a6cae44d968e3bc78dacc73c07a0f
abdiranman <abdirahman.abdi330@.com>
2023-08-25 09:57:26 -0400

    Create a Registration Handler

--- express_server.js
+++ express_server.js
@@ -11,16 +11,7 @@ const urlDatabase = {
 };
 
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
+  // ... Your users database entries
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,73 +34,49 @@ app.get("/", (req, res) => {
 
 // Display list of URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies.username };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
   res.render("urls_index", templateVars);
 });
 
-// Display individual URL
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL, username: req.cookies.username };
-  res.render("urls_show", templateVars);
-});
-
-// POST route to update a URL resource
-app.post("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const newLongURL = req.body.longURL;
-  urlDatabase[id] = newLongURL;
-  res.redirect("/urls");
-});
-
-// POST route to delete a URL resource
-app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect("/urls");
-});
-
-// GET route to display the form for creating new URLs
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new", { username: req.cookies.username });
-});
-
-// POST route to create a new URL resource
-app.post("/urls", (req, res) => {
-  const randomID = generateRandomString();
-  const longURL = req.body.longURL;
-  urlDatabase[randomID] = longURL;
-  res.redirect(`/urls/${randomID}`);
-});
-
-// Redirect short URLs to long URLs
-app.get("/u/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
-  if (longURL) {
-    res.redirect(longURL);
-  } else {
-    res.status(404).send("URL Not Found");
-  }
-});
-
-// Display login form
-app.get("/login", (req, res) => {
-  res.render("login", { username: req.cookies.username });
-});
-
-// Handle login form submission
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
-  res.redirect("/urls");
-});
+// ... Other routes
 
 // Display registration form
 app.get("/register", (req, res) => {
-  res.render("register", { username: req.cookies.username });
+  res.render("register");
 });
+
+// Handle user registration
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userId = generateRandomString();
+
+  if (!email || !password) {
+    res.status(400).send("Email and password cannot be empty");
+    return;
+  }
+
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      res.status(400).send("Email already registered");
+      return;
+    }
+  }
+
+  users[userId] = {
+    id: userId,
+    email: email,
+    password: password,
+  };
+
+  res.cookie("user_id", userId);
+  res.redirect("/urls");
+});
+
+// ... Other routes
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
