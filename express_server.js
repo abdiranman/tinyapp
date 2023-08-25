e2e8a47e38720dac517148cac1bcdc4241f0fe0c
abdiranman <abdirahman.abdi330@.com>
2023-08-25 11:34:29 -0400

    Short URLs

--- express_server.js
+++ express_server.js
@@ -43,65 +43,20 @@ app.get("/urls", (req, res) => {
 
 // ... Other routes ...
 
-// Display login form
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-// Handle login form submission
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+// Display the long URL for a given short URL
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
   
-  for (const userID in users) {
-    const user = users[userID];
-    if (user.email === email && user.password === password) {
-      res.cookie("user_id", user.id);
-      res.redirect("/urls");
-      return;
-    }
-  }
-  
-  res.status(403).send("Invalid email or password");
-});
-
-// Handle logout
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
-  res.redirect("/login");
-});
-
-// Display registration form
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-// Handle registration form submission
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  
-  if (!email || !password) {
-    res.status(400).send("Email and password fields cannot be empty");
+  if (!longURL) {
+    res.status(404).send("Short URL not found");
     return;
   }
-  
-  for (const userID in users) {
-    if (users[userID].email === email) {
-      res.status(400).send("Email already exists");
-      return;
-    }
-  }
-  
-  const randomID = generateRandomString();
-  users[randomID] = {
-    id: randomID,
-    email: email,
-    password: password
-  };
-  res.cookie("user_id", randomID);
-  res.redirect("/urls");
+
+  res.redirect(longURL);
 });
+
+// ... Other routes ...
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
