ed3438dd9b597d9326189dbffc2ccd31d00f616c
abdiranman <abdirahman.abdi330@.com>
2023-08-25 10:44:58 -0400

    Update the Login Handler

--- express_server.js
+++ express_server.js
@@ -1,34 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+// Example URL database
 const urlDatabase = {
-  // ... Your URL database entries
+  "b2xVn2": "http://www.example.com",
+  "9sm5xK": "http://www.google.com"
 };
 
+// Example user database
 const users = {
-  // ... Your user objects
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "$2b$10$TigO0F0b9rXBGU47HMRoi.QV9RzOK29JS.gucPOe3ISX2wLbnZoXq" // Hashed password
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2b$10$TigO0F0b9rXBGU47HMRoi.QV9RzOK29JS.gucPOe3ISX2wLbnZoXq" // Hashed password
+  }
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Function to generate a random string
-function generateRandomString() {
-  let randomStr = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 6; i++) {
-    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return randomStr;
-}
+// ... (Other routes)
 
-// Function to find a user by email
-function findUserByEmail(email) {
+// Display login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Handle login form submission
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  const user = getUserByEmail(email);
+
+  if (!user) {
+    res.status(403).send("Invalid email or password");
+    return;
+  }
+
+  // Compare hashed passwords
+  if (!bcrypt.compareSync(password, user.password)) {
+    res.status(403).send("Invalid email or password");
+    return;
+  }
+
+  // Set user_id cookie and redirect
+  res.cookie("user_id", user.id);
+  res.redirect("/urls");
+});
+
+// ... (Other routes)
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
+});
+
+// Helper function to get user by email
+function getUserByEmail(email) {
   for (const userId in users) {
     if (users[userId].email === email) {
       return users[userId];
@@ -36,53 +76,3 @@ function findUserByEmail(email) {
   }
   return null;
 }
-
-// Home page
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-// Display list of URLs
-app.get("/urls", (req, res) => {
-  const user = users[req.cookies["user_id"]];
-  const templateVars = { urls: urlDatabase, user };
-  res.render("urls_index", templateVars);
-});
-
-// ... Other routes ...
-
-// Display registration form
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-// Handle registration form submission
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  // Error handling for empty email or password
-  if (!email || !password) {
-    return res.status(400).send("Email and password cannot be empty.");
-  }
-
-  // Check if the email already exists in users
-  if (findUserByEmail(email)) {
-    return res.status(400).send("Email already registered.");
-  }
-
-  const id = generateRandomString();
-  users[id] = {
-    id,
-    email,
-    password,
-  };
-  res.cookie("user_id", id);
-  res.redirect("/urls");
-});
-
-// ... Other routes ...
-
-app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
-});
