a129467bc9b0003a2b179f746cb2e2326e85aa01
abdiranman <abdirahman.abdi330@.com>
2023-08-25 12:04:12 -0400

    add

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const bcrypt = require("bcryptjs"); // Import bcryptjs library
+const bcrypt = require("bcryptjs"); // Require bcryptjs
 const app = express();
 const PORT = 8080;
 
@@ -16,11 +16,16 @@ const users = {
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser()); // Use cookie-parser middleware
 
 // Function to generate a random string
 function generateRandomString() {
-  // ... (same as before)
+  let randomStr = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomStr;
 }
 
 // Home page
@@ -30,9 +35,12 @@ app.get("/", (req, res) => {
 
 // Display list of URLs
 app.get("/urls", (req, res) => {
-  // ... (same as before)
-});
-
+    const templateVars = {
+      urls: urlDatabase,
+      user: users[req.cookies['user_id']]
+    };
+    res.render("urls_index", templateVars);
+  });
 // ... Other routes ...
 
 // Display login form
@@ -44,44 +52,53 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-
+  
   for (const userID in users) {
     const user = users[userID];
-    if (user.email === email && bcrypt.compareSync(password, user.password)) {
+    if (user.email === email && bcrypt.compareSync(password, user.password)) { // Use bcrypt.compareSync
       res.cookie("user_id", user.id);
       res.redirect("/urls");
       return;
     }
   }
-
+  
   res.status(403).send("Invalid email or password");
 });
 
-// ... (other routes)
+// Handle logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/login");
+});
+
+// Display registration form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 // Handle registration form submission
 app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-
+  
   if (!email || !password) {
     res.status(400).send("Email and password fields cannot be empty");
     return;
   }
-
+  
   for (const userID in users) {
     if (users[userID].email === email) {
       res.status(400).send("Email already exists");
       return;
     }
   }
-
+  
   const randomID = generateRandomString();
-  const hashedPassword = bcrypt.hashSync(password, 10); // Hash the password
+  const hashedPassword = bcrypt.hashSync(password, 10); // Use bcrypt to hash the password
   users[randomID] = {
     id: randomID,
     email: email,
-    password: hashedPassword, // Save the hashed password
+    password: hashedPassword, // Store the hashed password
   };
   res.cookie("user_id", randomID);
   res.redirect("/urls");
